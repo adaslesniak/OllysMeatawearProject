1d0e2308c5533e7110bf93781df0066437226cfa
Adas Lesniak <radical.ignorant@gmail.com>
2019-04-04 22:12:20 +0200

    Initial Commit

---
+++ OllysProject/ViewController.swift
@@ -0,0 +1,15 @@
+// ViewController.swift [] created by: Adas Lesniak on: 04/04/2019 
+// Copyright ©Aulendil   All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
